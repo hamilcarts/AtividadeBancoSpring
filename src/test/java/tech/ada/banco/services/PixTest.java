9c2c4e2dece0dabcbf63abb18c3f8a1e4a55a7a3
Hamilcar Tortora da Silva <109037178+hamilcarts@users.noreply.github.com>
2023-02-13 18:42:06 -0300

    Commit PixTest 2

--- src/test/java/tech/ada/banco/services/PixTest.java
+++ src/test/java/tech/ada/banco/services/PixTest.java
@@ -3,6 +3,7 @@ package tech.ada.banco.services;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import tech.ada.banco.exceptions.ResourceNotFoundException;
+import tech.ada.banco.exceptions.SaldoInsuficienteException;
 import tech.ada.banco.model.Conta;
 import tech.ada.banco.model.ModalidadeConta;
 import tech.ada.banco.repository.ContaRepository;
@@ -28,7 +29,7 @@ class PixTest {
         pix.executar(10, 11, BigDecimal.valueOf(7));
         assertEquals(BigDecimal.valueOf(3), conta1.getSaldo(), "O saldo da conta deve ser alterado para 3");
         assertEquals(BigDecimal.valueOf(8), conta2.getSaldo(), "O saldo da conta deve ser alterado para 8");
-        // valor normal positivo
+
 
     }
     @Test
@@ -40,8 +41,8 @@ class PixTest {
         conta1.deposito( BigDecimal.TEN);
         conta2.deposito(BigDecimal.ONE);
         pix.executar(10, 11, BigDecimal.valueOf(7.3));
-        assertEquals(BigDecimal.valueOf(2.7), conta1.getSaldo(), "O saldo da conta deve ser alterado para 3");
-        assertEquals(BigDecimal.valueOf(8.3), conta2.getSaldo(), "O saldo da conta deve ser alterado para 8");
+        assertEquals(BigDecimal.valueOf(2.7), conta1.getSaldo(), "O saldo da conta deve ser alterado para 2.7");
+        assertEquals(BigDecimal.valueOf(8.3), conta2.getSaldo(), "O saldo da conta deve ser alterado para 8.3");
 
     }
     @Test
@@ -80,14 +81,20 @@ class PixTest {
     void pixContaSemSaldo() {
         Conta conta1 = new Conta(ModalidadeConta.CC, null);
         Conta conta2 = new Conta(ModalidadeConta.CC, null);
-
-        fail();
+        when(repository.findContaByNumeroConta(10)).thenReturn(Optional.of(conta1));
+        when(repository.findContaByNumeroConta(11)).thenReturn(Optional.of(conta2));
+        assertThrows(SaldoInsuficienteException.class, () -> pix.executar(10, 11,BigDecimal.valueOf(6)));
     }
     @Test
     void pixNegativo() {
         Conta conta1 = new Conta(ModalidadeConta.CC, null);
         Conta conta2 = new Conta(ModalidadeConta.CC, null);
-
-        fail();
+        when(repository.findContaByNumeroConta(10)).thenReturn(Optional.of(conta1));
+        when(repository.findContaByNumeroConta(11)).thenReturn(Optional.of(conta2));
+        conta1.deposito( BigDecimal.TEN);
+        conta2.deposito(BigDecimal.ONE);
+        assertThrows(IllegalArgumentException.class, () -> pix.executar(10, 11,BigDecimal.valueOf(-7)));
+        assertEquals(BigDecimal.valueOf(10), conta1.getSaldo(), "O saldo da conta deve ser 10");
+        assertEquals(BigDecimal.valueOf(1), conta2.getSaldo(), "O saldo da conta deve ser 1");
     }
 }
